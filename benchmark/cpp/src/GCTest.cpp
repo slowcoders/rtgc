87a787af28dfafc757b744be5e63be216633a0f5
zeedhoon <zeedhoon@gmail.com>
2026-08-09 16:19:42 +0900

    test decreaseRefCount

--- benchmark/cpp/src/GCTest.cpp
+++ benchmark/cpp/src/GCTest.cpp
@@ -7,156 +7,113 @@
 
 struct RCObj {
     int idx;
-    int cntIncrement;
     atomic_int refCount;
 };
 
-const int CNT_OBJ = 2048;
-const int TEST_LOOP = 100*1000*100;
+const int CNT_OBJ = 2048 * 10;
+const int TEST_THREAD = 20;
+const int TEST_LOOP = 1000 * 10;
 
 struct Container {
     RCObj* obj;
 };
 
-volatile Container sharedObj;
+Container sharedObj;
 
-void* testLoop_noExhg(void* argument) {
-    RCObj* rcObjs = (RCObj*)argument;
-    for (int i = 0; i < CNT_OBJ; i ++) { 
-        rcObjs[i].idx = i;
-        rcObjs[i].cntIncrement = 0;
-        rcObjs[i].refCount = 0;        
-    }
+#define HOOK_ADDR 0
 
-    for (int k = 0; k < TEST_LOOP; k ++) {
-        for (int i = 0; i < CNT_OBJ; i ++) {
-            while (true) {
-                RCObj* old = sharedObj.obj;
-                int old_rc = old == NULL ? -1 : atomic_load_explicit(&old->refCount, memory_order::memory_order_acquire);
-                if (sharedObj.obj != old) {
-                    // rcObjs[0].idx ++;
-                    continue;
-                }
+__attribute__((noinline)) RCObj* replace_in_heap(RCObj** addr, RCObj* value) {
+    while (true) {
+        RCObj* old = *addr;
+        int old_rc = old == NULL ? -1 : 
+            (HOOK_ADDR ? old->refCount : atomic_load_explicit(&old->refCount, memory_order::memory_order_acquire));
 
-                RCObj* b = rcObjs + i;
-                b->refCount = 0;
-                sharedObj.obj = b;
-                if (old_rc >= 0) {
-                    // assert(old_rc == 0);
-                    atomic_store_explicit(&old->refCount, old_rc + 1, memory_order::memory_order_release);
-                }
-                break;
-            }
+#if HOOK_ADDR            
+        if ((RCObj*)atomic_load_explicit((atomic_intptr_t*)addr, memory_order::memory_order_acquire) != old) {
+            continue;
         }
-        sharedObj.obj = NULL;
-        // assign_ref(&dummy);
-        for (int i = 0; i < CNT_OBJ; i ++) { 
-            // assert(rcObjs[i].refCount <= 1);
-            // rcObjs[i].refCount = 0;
+        atomic_store_explicit((atomic_intptr_t*)addr, (intptr_t)value, memory_order::memory_order_release);
+#else
+        if (*addr != old) continue;
+        *addr = value;
+#endif
+
+        if (old_rc >= 0) {
+            if (old_rc == 0) {
+                printf("addr: %p old: %p\n", addr, old);
+                assert(old_rc > 0);
+            }
+#if HOOK_ADDR            
+            old->refCount = old_rc - 1;
+#else 
+            atomic_store_explicit(&old->refCount, old_rc - 1, memory_order::memory_order_release);
+#endif
+        }
+        return old;
+    }
+}
+
+void replace_not_in_heap(RCObj** addr, RCObj* value) {
+    RCObj* old = atomic_exchange_explicit((_Atomic(RCObj*)*)addr, value, memory_order::memory_order_seq_cst);
+    if (old != value) {
+        atomic_fetch_add(&value->refCount, 1);
+        if (old != NULL) {
+            atomic_fetch_sub(&old->refCount, 1);
         }
     }
 }
 
-void* testLoop_exhg(void* argument) {
-    RCObj* rcObjs = (RCObj*)argument;
-    for (int i = 0; i < CNT_OBJ; i ++) { 
-        rcObjs[i].idx = i;
-        rcObjs[i].cntIncrement = 0;
-        rcObjs[i].refCount = 0;        
-    }
+atomic_int cntTest = 0;
+void* testLoop(void* argument) {
+    RCObj rcObjs[CNT_OBJ];
+    const bool use_usleep = true;
 
+    atomic_fetch_add(&cntTest, 1);
     for (int k = 0; k < TEST_LOOP; k ++) {
-        for (int i = 0; i < CNT_OBJ; i ++) {
-            while (true) {
-                RCObj* b = rcObjs + i;
-                b->refCount = 0;
-                RCObj* old = atomic_exchange_explicit((_Atomic(RCObj*)*)&sharedObj.obj, b, memory_order::memory_order_seq_cst);
-                if (old != NULL) {
-                    old->refCount ++;
-                }
-                break;
-            }
+        for (int i = 0; i < CNT_OBJ; i++) {
+            rcObjs[i].refCount = 1;
+            replace_in_heap(&sharedObj.obj, &rcObjs[i]);
         }
-        sharedObj.obj = NULL;
-        // assign_ref(&dummy);
-        for (int i = 0; i < CNT_OBJ; i ++) { 
-            // Fail: Over Decrement 발생!!
-            // assert(rcObjs[i].refCount <= 1);
-        }
+        replace_in_heap(&sharedObj.obj, nullptr);
+    }
+    atomic_fetch_sub(&cntTest, 1);
+    while (cntTest > 0) {
+        usleep(1000);
     }
 }
 
-void* testLoop_unsafe(void* argument) {
-    RCObj* rcObjs = (RCObj*)argument;
-    for (int i = 0; i < CNT_OBJ; i ++) { 
-        rcObjs[i].idx = i;
-        rcObjs[i].cntIncrement = 0;
-        rcObjs[i].refCount = 0;        
-    }
 
-    for (int k = 0; k < TEST_LOOP; k ++) {
-        for (int i = 0; i < CNT_OBJ; i ++) {
-            while (true) {
-                RCObj* old = sharedObj.obj;
-                int old_rc = old == NULL ? -1 : old->idx;
-                if (sharedObj.obj != old) {
-                    // rcObjs[0].idx ++;
-                    continue;
-                }
-
-                RCObj* b = rcObjs + i;
-                b->idx = 0;
-                sharedObj.obj = b;
-                if (old_rc >= 0) {
-                    // assert(old_rc == 0);
-                    old->idx = old_rc + 1;
-                }
-                break;
-            }
-        }
-        sharedObj.obj = NULL;
-        // assign_ref(&dummy);
-        for (int i = 0; i < CNT_OBJ; i ++) { 
-            // Fail: Over Decrement 발생!!
-            // assert(rcObjs[i].idx <= 1);
-        }
-    }
-}
 
 void doTestOverDecrement() {
-    RCObj tc0[CNT_OBJ];
-    RCObj tc1[CNT_OBJ];
-    RCObj tc2[CNT_OBJ];
-    RCObj tc3[CNT_OBJ];
+    RCObj rcObjs[CNT_OBJ];
+    sharedObj.obj = NULL;
+    for (int i = 0; i < CNT_OBJ; i ++) { 
+        rcObjs[i].idx = i;
+        rcObjs[i].refCount = 0;        
+    }
 
     printf("a_int32: %d, int32 %d\n", (int)sizeof(_Atomic(int32_t)), (int)sizeof(int32_t));
 
-    for (int type = 0; type < 3; type ++) {
-        sharedObj.obj = NULL;
+    
+    pthread_t threads[TEST_THREAD];
 
-        pthread_t t0, t1, t2, t3;
-        void * (* thread_main)(void *);
-        switch (type) {
-            case 0: thread_main = testLoop_unsafe; break;
-            case 1: thread_main = testLoop_noExhg; break;
-            case 2: thread_main = testLoop_exhg; break;
-        }
-        pthread_create(&t0, NULL, thread_main, tc0);
-        // pthread_create(&t1, NULL, thread_main, tc1);
-        // pthread_create(&t2, NULL, thread_main, tc2);
-        // pthread_create(&t3, NULL, thread_main, tc3);
-        
-        SimpleTimer timer;
-        timer.reset();
-        void* thread_result;
-        pthread_join(t0, &thread_result);
-        // pthread_join(t1, &thread_result);
-        // pthread_join(t2, &thread_result);
-        // pthread_join(t3, &thread_result);
-        int elapsed = timer.reset();
-        printf ("[tp:%d] elapsed: %d collision: %d\n", type, elapsed, tc0->idx + tc1->idx + tc2->idx + tc3->idx);
-        tc0->idx = tc1->idx = tc2->idx = tc3->idx = 0;
+    for (int i = 0; i < TEST_THREAD; i ++) {
+        pthread_create(&threads[i], NULL, testLoop, rcObjs);
     }
+    SimpleTimer timer;
+    timer.reset();
+    void* thread_result;
+    for (int i = 0; i < TEST_THREAD; i ++) {
+        pthread_join(threads[i], &thread_result);
+    }
+    int elapsed = timer.reset();
+
+    for (int i = 0; i < CNT_OBJ; i ++) { 
+        if (rcObjs[i].refCount < 0) {
+            printf("%d: %d\n", i, rcObjs[i].refCount);        
+        }
+    }
+    printf ("[tp:%d] elapsed: %d collision: %d\n", 0, elapsed, 0);
 
     // 단일 쓰레드 (no collision) Test 결과.
     // [unsafe]          elapsed:  8352 collision: 0
